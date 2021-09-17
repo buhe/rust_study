e1990761141c6cabeeb293b3d694def644457723
buhe <hare.bu@herewhite.com>
2021-09-17 17:39:21 +0800

    update

--- study/src/main.rs
+++ study/src/main.rs
@@ -1,3 +1,11 @@
+use std::sync::Mutex;
+
+struct Demo {
+   name: Mutex<i32>,
+}
 fn main() {
-    println!("Hello, world!");
+    let demo = Demo {name: Mutex::new(5)};
+    let n = demo.name.lock().unwrap();
+    println!("e={:}", n);
+    println!("e2={:}", n);
 }
