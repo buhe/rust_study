dd3fbb84c5f712c3e849881aa2bb8e80161ef518
buhe <hare.bu@herewhite.com>
2021-09-18 09:30:38 +0800

    Update main.rs

--- study/src/main.rs
+++ study/src/main.rs
@@ -1,11 +1,16 @@
 use std::sync::Mutex;
-
+#[derive(Debug)]
 struct Demo {
    name: Mutex<i32>,
 }
 fn main() {
     let demo = Demo {name: Mutex::new(5)};
-    let n = demo.name.lock().unwrap();
-    println!("e={:}", n);
-    println!("e2={:}", n);
+    // let n = demo.name.lock().unwrap();
+    // println!("e={:}", n);
+    // println!("e2={:}", n);
+    println!("{} days.", 31);
+
+    println!("{:b} ", 31);
+
+    println!("{:?}", demo);
 }
