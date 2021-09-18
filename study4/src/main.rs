555eddb5444fbd4a8d483f259ed0aa48c8fda148
buhe <hare.bu@herewhite.com>
2021-09-18 18:56:51 +0800

    Update main.rs

--- study4/src/main.rs
+++ study4/src/main.rs
@@ -7,5 +7,14 @@ fn main() {
         break;
     }
     // 嵌套循环也不是必须需要 label
-    
+    let mut a = vec![1 ,2 ,3 ];
+    let i = a.iter_mut();
+    for s in i {
+        *s = match s  {
+            &mut 2 => 6,
+            _ => 5,
+        }
+    }
+    println!("a is {:?}", a);
+   
 }
