feec8cec6a9679eb9df76032cd9b9f65dab6f620
buhe <hare.bu@herewhite.com>
2021-09-18 15:30:46 +0800

    Update main.rs

--- study3/src/main.rs
+++ study3/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Point {
     x: f32,
     y: f32,
@@ -5,6 +6,7 @@ struct Point {
 
 // 结构体可以作为另一个结构体的字段
 #[allow(dead_code)]
+#[derive(Debug)]
 struct Rectangle {
     p1: Point,
     p2: Point,
@@ -14,7 +16,11 @@ fn rect_area(r: Rectangle) -> f32{
     let (w,h) = (r.p1.x - 0f32, r.p2.y - 0f32 ) ;
     w * h
 }
+fn square(f: f32, p: Point) -> Rectangle {
+    Rectangle{p1: Point{x: p.x,y: p.y - f}, p2: Point{x: p.x - f, y: p.y}}
+}
 fn main() {
     println!("Hello, world!");
-    println!("rect area is {}", rect_area(Rectangle{p1: Point{x: 1.1, y: 1.2}, p2: Point{x: 2.1, y: 2.2}}))
+    println!("rect area is {}", rect_area(Rectangle{p1: Point{x: 1.1, y: 1.2}, p2: Point{x: 2.1, y: 2.2}}));
+    println!("square is {:?}", square(1.1, Point { x: 2.1, y: 2.2 }));
 }
